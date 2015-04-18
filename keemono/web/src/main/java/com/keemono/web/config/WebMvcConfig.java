052ec335c04531073555d9190aea30c0246b2222
admin <admin@samuris.com>
2015-04-18 13:46:12 +0200

    KB-1 added more pom definitions. started web module implementation

---
+++ keemono/web/src/main/java/com/keemono/web/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package com.keemono.web.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * Created by edu on 18/4/15.
+ */
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+}
