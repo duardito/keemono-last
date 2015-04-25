f8018395f3a01ea57844923d2a007878ed939b0b
admin <admin@samuris.com>
2015-04-25 16:01:29 +0200

    KB-3

--- keemono/integration/src/main/java/com/keemono/integration/page/PageController.java
+++ keemono/integration/src/main/java/com/keemono/integration/page/PageController.java
@@ -1,13 +1,23 @@
 package com.keemono.integration.page;
 
+import com.keemono.common.request.converter.page.PageResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by edu on 25/04/2015.
  */
+@RequestMapping(value = "/page")
 @Controller
 public class PageController {
 
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
+    public PageResponse getPage(@PathVariable String id){
 
+        PageResponse pageResponse = null;
+        return pageResponse;
+    }
 
 }
