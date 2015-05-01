f3fa8b64eb9a3d6b802ee7363973dce16cef1d7d
admin <admin@samuris.com>
2015-05-01 20:33:04 +0200

    KB-8 added first infraestructure to add jpa, using hickari instead c3p0

--- keemono/web/src/main/java/com/keemono/web/WebConfiguration.java
+++ keemono/web/src/main/java/com/keemono/web/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.keemono.web;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * Created by edu on 18/04/2015.
  */
 @EnableTransactionManagement
-//@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "com.keemono.domain.mysql.repository")
 @EnableMongoRepositories(basePackages = "com.keemono.domain.mongo.*.repository")
 @PropertySource("classpath:config/mongo.properties")
 @ComponentScan(basePackages = {"com.keemono.web","com.keemono.integration","com.keemono.core", "com.keemono.configuration.mongo"} )
