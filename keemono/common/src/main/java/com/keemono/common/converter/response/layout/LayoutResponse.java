24312ee7bb81a09c5bf3e70e5ec3905cb3bf7baf
admin <admin@samuris.com>
2015-08-29 16:36:33 +0200

    added swager annotations

--- keemono/common/src/main/java/com/keemono/common/converter/response/layout/LayoutResponse.java
+++ keemono/common/src/main/java/com/keemono/common/converter/response/layout/LayoutResponse.java
@@ -1,11 +1,13 @@
 package com.keemono.common.converter.response.layout;
 
 import com.keemono.common.converter.base.AbstractResponse;
+import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by edu on 10/05/2015.
  */
+@ApiModel(value = "response" , description = "layout response")
 public class LayoutResponse extends AbstractResponse {
 
     @ApiModelProperty(value = "schema layout", required = true)
