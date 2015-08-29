f87cdfae4759f4a86842753f084899403300ce0c
admin <admin@samuris.com>
2015-08-29 16:31:11 +0200

    added swager annotations

--- keemono/common/src/main/java/com/keemono/common/converter/base/AbstractResponse.java
+++ keemono/common/src/main/java/com/keemono/common/converter/base/AbstractResponse.java
@@ -1,7 +1,10 @@
 package com.keemono.common.converter.base;
 
+import com.wordnik.swagger.annotations.ApiModel;
+
 /**
  * Created by edu on 25/04/2015.
  */
+@ApiModel(value = "abstract request" , description = "abstract response")
 public abstract class AbstractResponse {
 }
