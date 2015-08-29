17c45801cfff5f7fdc365eb4d29d0d562624d365
admin <admin@samuris.com>
2015-08-29 16:21:02 +0200

    added swager annotations

--- keemono/common/src/main/java/com/keemono/common/converter/request/layout/LayoutRequest.java
+++ keemono/common/src/main/java/com/keemono/common/converter/request/layout/LayoutRequest.java
@@ -1,12 +1,13 @@
 package com.keemono.common.converter.request.layout;
 
 import com.keemono.common.converter.base.AbstractRequest;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 
-/**
- * Created by edu on 10/05/2015.
- */
+@ApiModel(value = "layout request" , description = "layout values coming from request")
 public class LayoutRequest extends AbstractRequest {
 
+    @ApiModelProperty(value = "schema layout", required = true)
     private String schema;
 
     public String getSchema() {
