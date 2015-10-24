ad188905f4d389c437b2923a5054162d99d0a328
admin <admin@samuris.com>
2015-10-24 15:23:16 +0200

    KB-17

---
+++ keemono/web/src/test/java/com/keemono/layout/myTest.java
@@ -0,0 +1,16 @@
+package com.keemono.layout;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by edu on 24/10/2015.
+ */
+public class myTest {
+
+    @Test
+    public void myTest(){
+        Assert.assertEquals(1, 1);
+    }
+}
