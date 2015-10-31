47aef93b4c495a3232e246afdca7f3ed845f6d7d
admin <admin@samuris.com>
2015-10-31 12:05:11 +0100

    KB-18 renamed name column

--- keemono/web/src/test/resources/config/create-ddl.sql
+++ keemono/web/src/test/resources/config/create-ddl.sql
@@ -12,7 +12,7 @@ CREATE TABLE `user` (
 CREATE TABLE `layout` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `schema` varchar(2000) NOT NULL,
+  `data` varchar(2000) NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `uuid` varchar(36) NOT NULL,
   PRIMARY KEY (`id`),
@@ -27,4 +27,4 @@ insert into user(id,name,uuid,lastname,email) values (1, 'edu','c695b78b-7218-4e
 insert into user(id,name,uuid,lastname,email) values (2,'juan','1063ac98-89c4-447c-aa08-c8ce354f809a','perez','juan.perez@gmail.com');
 
 
-INSERT INTO layout(id,name,valores,user_id,uuid ) VALUES (1,'two-layout','<div id="2" class="row"><div id="3" class="col-xs-4"><div id="4" class="dragable"></div></div><div id="5" class="col-xs-8"><div id="6" class="dragable"></div></div></div>',1,'c695b78b-7218-4e53-897b-51d29c250911');
+INSERT INTO layout(id,name,data,user_id,uuid ) VALUES (1,'two-layout','<div id="2" class="row"><div id="3" class="col-xs-4"><div id="4" class="dragable"></div></div><div id="5" class="col-xs-8"><div id="6" class="dragable"></div></div></div>',1,'c695b78b-7218-4e53-897b-51d29c250911');
