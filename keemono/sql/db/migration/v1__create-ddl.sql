37c5f50c45785eaaf18b768e07a632bab914948f
admin <admin@samuris.com>
2016-03-05 09:18:34 +0100

    KB-29 1.added flyway 2.correct test mysql values

--- keemono/sql/db/migration/v1__create-ddl.sql
+++ keemono/sql/db/migration/v1__create-ddl.sql
@@ -35,8 +35,3 @@ CREATE TABLE `page` (
 ) ENGINE=InnoDB;
 
 
-insert into user(id,name,uuid,lastname,email) values (1, 'edu','c695b78b-7218-4e53-897b-51d29c250965','frades','eduard.frades@gmail.com');
-insert into user(id,name,uuid,lastname,email) values (2,'juan','1063ac98-89c4-447c-aa08-c8ce354f809a','perez','juan.perez@gmail.com');
-
-
-INSERT INTO layout(id,name,data,user_id,uuid ) VALUES (1,'two-layout','<div id="2" class="row"><div id="3" class="col-xs-4"><div id="4" class="dragable"></div></div><div id="5" class="col-xs-8"><div id="6" class="dragable"></div></div></div>',1,'c695b78b-7218-4e53-897b-51d29c250911');
